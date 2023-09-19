4f3d0167788ab309a52fb3fd82be032866603817
Fanyam Terkuma <lawrencefanyam@gmail.com>
2023-09-19 20:51:48 +0100

    INITIAL SETUP AND BOILERPLATE CLEANUP. 1. Downloaded and assets (images and Clash display font). 2. Remove boilerplate files that  don't need.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        colors: {
+            blue: '#150E28',
+            purple: '#903AFF',
+            pink: '#D434FE',
+            pinker: '#FE34B9'
+        },
+        extend: {}
+    },
+    plugins: []
+};
